d2318de392928548a5c18a645f9866fe024fc7d2
Moein Madadi <moeinmadadi2002@gmail.com>
2024-01-01 11:22:21 +0330

    default

---
+++ lib/layers/base/utils.py
@@ -0,0 +1,20 @@
+import collections.abc
+from itertools import repeat
+
+
+def _ntuple(n):
+
+    def parse(x):
+        # if isinstance(x, container_abcs.Iterable):
+        if isinstance(x, collections.abc.Iterable):
+            return x
+        return tuple(repeat(x, n))
+
+    return parse
+
+
+_single = _ntuple(1)
+_pair = _ntuple(2)
+_triple = _ntuple(3)
+_quadruple = _ntuple(4)
+
